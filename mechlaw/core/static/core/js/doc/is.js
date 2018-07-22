dc6cf46042ae7c4bc4bd464d36367290dcc2af40
Helgi Hrafn Gunnarsson <helgi@binary.is>
2018-07-22 17:21:26 +0000

    Support for highlighting a range (and some code refactoring and commenting).

--- mechlaw/core/static/core/js/doc/is.js
+++ mechlaw/core/static/core/js/doc/is.js
@@ -11,34 +11,60 @@ $(document).ready(function() {
         $changing_law.find('changed-portion').each(function() {
             var $portion = $(this);
 
-            // Start with the entire law and narrow it down later. $mark is
-            // perhaps not a very well named variable, but at any rate, it
-            // means the section of text in the law, that was changed.
-            var $mark = $portion.parent().parent().closest('law');
+            // Start with the entire law and narrow it down later. The
+            // variables $start_mark and $end_mark denote the highlight's
+            // start point and end point, respectively. In the beginning, they
+            // are both set to the entire legal document's XML, but then
+            // narrowed down afterwards. This means that each node in
+            // <changed-portion> narrows it down by one step, until eventually
+            // we have the final node, which is the one where the highlighting
+            // should take place.
+            //
+            // Usually, the $start_mark and $end_mark end
+            // up being the same thing. They are only different when the
+            // highlight covers a range of entities.
+            var $start_mark = $portion.parent().parent().closest('law');
+            var $end_mark = $portion.parent().parent().closest('law');
 
             // Iterate through the <changed-portion> section to locate the
             // text that we want to show as changed.
             $portion.children().each(function() {
-                var $child = $(this);
+                var $location_node = $(this);
 
-                var tag_name = $child.prop('tagName').toLowerCase();
-                var value = $child.text();
+                var tag_name = $location_node.prop('tagName').toLowerCase();
+                var value = $location_node.text();
+
+                // Check if the value is a range, for example "1-4", which
+                // would mean that the highlighting should start at entity nr.
+                // 1 (for example, article 1) and end at entity nr. 4 (for
+                // example, article 4).
+                var minus_index = value.indexOf('-');
+                if (minus_index > -1) {
+                    var start_i = value.substring(0, minus_index);
+                    var end_i = value.substring(minus_index + 1);
+                }
+                else {
+                    var start_i = value;
+                    var end_i = value;
+                }
 
                 // For every tag we find, we look deeper into the XML.
-                $mark = $mark.find(tag_name + '[nr="' + value + '"]');
-                $mark.tag = $child;
+                $start_mark = $start_mark.find(tag_name + '[nr="' + start_i + '"]');
+                $end_mark = $end_mark.find(tag_name + '[nr="' + end_i + '"]');
+                $start_mark.location_node = $location_node;
+                $end_mark.location_node = $location_node;
 
             });
 
-            // Add the markers for changed text and enumerate the changes according to order.
-            var tag_name = $mark.prop('tagName').toLowerCase();
+            // Add markers to denote the highlighted area.
+            var tag_name = $start_mark.prop('tagName').toLowerCase(); // $start_mark arbitrarily chosen.
             if (tag_name == 'art' || tag_name == 'subart' || tag_name == 'numart') {
-                var words = $mark.tag.attr('words');
+                var words = $start_mark.location_node.attr('words');
 
                 if (words) {
                     // If specific words are to be highlighted, we'll just
                     // replace them with themselves, highlighted.
-                    $mark.html($mark.html().replace(
+                    $start_mark.html($start_mark.html().replace(
                         words,
                         '[' + words + ']<sup>' + change_number + '</sup> '
                     ));
@@ -47,14 +73,14 @@ $(document).ready(function() {
                     // Otherwise, we'll highlight the last mark. If there is a
                     // <nr-title> tag, we'll want to skip that, so that the
                     // opening bracket is placed right after it.
-                    var $nr_title = $mark.find('nr-title');
+                    var $nr_title = $start_mark.find('nr-title');
                     if ($nr_title.length > 0) {
-                        $mark.find('nr-title').next().first().prepend('[');
+                        $start_mark.find('nr-title').next().first().prepend('[');
                     }
                     else {
-                        $mark.children().first().prepend('[');
+                        $start_mark.children().first().prepend('[');
                     }
-                    $mark.find('sen').last().append(' ]<sup>' + change_number + '</sup> ');
+                    $end_mark.find('sen').last().append(' ]<sup>' + change_number + '</sup> ');
                 }
             }
         });
