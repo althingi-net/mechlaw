b19b35caeb3c609812f8c8c8133a2bd0611d4da6
Helgi Hrafn Gunnarsson <helgi@binary.is>
2018-07-23 00:27:07 +0000

    Footnote processing now supports checking for the implicit order of elements instead of relying on explicitly set ones, which means that it supports the <sen> tag as well. (<art> now works as well as <art nr="2">).

--- mechlaw/core/static/core/js/doc/is.js
+++ mechlaw/core/static/core/js/doc/is.js
@@ -57,11 +57,33 @@ var process_footnote = function() {
                     var end_i = nr;
                 }
 
-                $start_mark = $start_mark.find(tag_name + '[nr="' + start_i + '"]');
-                $end_mark = $end_mark.find(tag_name + '[nr="' + end_i + '"]');
+                // If we cannot find an element explicitly numbered according
+                // to what's being requested, we will try to figure it out.
+                //
+                // Note that our iterators, start_i and end_i, actually start
+                // at 1 instead of 0.
+                $start_mark_attempt = $start_mark.find(tag_name + '[nr="' + start_i + '"]');
+                $end_mark_attempt = $end_mark.find(tag_name + '[nr="' + end_i + '"]');
+
+                // If the attempt did not result in a valid node, it means
+                // that there is none explicitly ordered as the one requested.
+                if ($start_mark_attempt.prop('tagName')) {
+                    $start_mark = $start_mark_attempt;
+                }
+                else {
+                    $start_mark = $start_mark.find(tag_name + ':nth-child(' + start_i + ')');
+                }
+
+                // Same story as with $start_mark above.
+                if ($end_mark_attempt.prop('tagName')) {
+                    $end_mark = $end_mark_attempt;
+                }
+                else {
+                    $end_mark = $end_mark.find(tag_name + ':nth-child(' + end_i + ')');
+                }
             }
             else {
-                // If no number is specified (f.e. article 3 or subarticle 5),
+                // If no number is requested (f.e. article 3 or subarticle 5),
                 // we will assume the first node with the given tag name.
                 $start_mark = $start_mark.find(tag_name).first();
                 $end_mark = $end_mark.find(tag_name).first();
@@ -72,74 +94,79 @@ var process_footnote = function() {
 
         });
 
+        // Let's be nice to XML writers and tell them what's wrong.
         if (!$start_mark.prop('tagName')) {
             var art_num = $location.closest('art').attr('nr');
             console.error('Invalid location in footnote nr. ' + footnote_num + ' in article nr. ' + art_num);
             return;
         }
 
-        // Add markers to denote the highlighted area.
+        /***********************************************/
+        /* Add markers to denote the highlighted area. */
+        /***********************************************/
+
+        // Short-hands.
         var tag_name = $start_mark.prop('tagName').toLowerCase(); // $start_mark arbitrarily chosen.
-        if (tag_name == 'art' || tag_name == 'subart' || tag_name == 'numart' || tag_name == 'name') {
-            var location_type = $location.attr('type');
-            var words = $start_mark.location_node.attr('words');
+        var location_type = $location.attr('type');
+        var words = $start_mark.location_node.attr('words');
 
-            if (words) {
+        if (words) {
 
-                if (location_type == 'range') {
-                    replace_text = '[' + words + '] <sup>' + footnote_num + ')</sup> '
-                }
-                else if (location_type == 'point') {
-                    replace_text = words + ' <sup>' + footnote_num + ')</sup>'
-                }
-
-                // If specific words are to be highlighted, we'll just replace
-                // them with themselves, highlighted.
-                $start_mark.html($start_mark.html().replace(
-                    words,
-                    replace_text
-                ));
+            // If specific words are specified, things get really simple and
+            // we can just replace the existing text with itself plus the
+            // relevant symbols for denoting ranges and points.
+            if (location_type == 'range') {
+                replace_text = '[' + words + '] <sup>' + footnote_num + ')</sup> '
             }
-            else {
-                if (location_type == 'range') {
-                    // If there is a <nr-title> tag, we'll want to skip that,
-                    // so that the opening bracket is placed right after it.
-                    var $nr_title = $start_mark.find('> nr-title');
-                    if ($nr_title.length > 0) {
-                        $start_mark.find('nr-title').next().first().prepend('[');
-                    }
-                    else {
-                        // Most entities, like articles, subarticles, numeric
-                        // articles and such have children nodes which contain
-                        // content. An occasional node does not, for example
-                        // <name>. We therefore check if the node has children
-                        // nodes, and if so, we will call the first one an
-                        // opening node and append the opening bracket, but
-                        // otherwise we'll use the $start_mark itself as the
-                        // opening node.
-                        var $opening_node = $start_mark.children().first();
-                        if (!$opening_node.prop('tagName')) {
-                            $opening_node = $start_mark;
-                        }
-                        $opening_node.prepend('[');
-                    }
-
-                    append_closing_text = '] <sup>' + footnote_num + ')</sup> ';
-                }
-                else if (location_type == 'point') {
-                    append_closing_text = ' <sup>' + footnote_num + ')</sup>'
-                }
-
-                // Like with the opening node, we'll need to check if the
-                // $end_mark has children to which the closing bracket should
-                // be appended. If not, we'll append the closing bracket to
-                // $end_node itself.
-                var $closing_node = $end_mark.find('sen').last();
-                if (!$closing_node.prop('tagName')) {
-                    $closing_node = $end_mark;
-                }
-                $closing_node.append(append_closing_text);
+            else if (location_type == 'point') {
+                replace_text = words + ' <sup>' + footnote_num + ')</sup>'
             }
+
+            $start_mark.html($start_mark.html().replace(
+                words,
+                replace_text
+            ));
+        }
+        else {
+            if (location_type == 'range') {
+                // If there is a <nr-title> tag, we'll want to skip that, so
+                // that the opening bracket is placed right after it.
+                var $nr_title = $start_mark.find('> nr-title');
+                if ($nr_title.length > 0) {
+                    $start_mark.find('nr-title').next().first().prepend('[');
+                }
+                else {
+                    // Most entities, like articles, subarticles, numeric
+                    // articles and such have children <sen> nodes which
+                    // contain the content. An occasional node does not, for
+                    // example <name> and of course <sen> itself. We therefore
+                    // check if the node has any <sen> children nodes, and if
+                    // so, we will call the first one an opening node and
+                    // append the opening bracket, but otherwise we'll use the
+                    // $start_mark itself as the opening node, since it must
+                    // then be one of the special nodes like <name> or <sen>.
+                    var $opening_node = $start_mark.find('sen').first();
+                    if (!$opening_node.prop('tagName')) {
+                        $opening_node = $start_mark;
+                    }
+                    $opening_node.prepend('[');
+                }
+
+                append_closing_text = '] <sup>' + footnote_num + ')</sup> ';
+            }
+            else if (location_type == 'point') {
+                append_closing_text = ' <sup>' + footnote_num + ')</sup>'
+            }
+
+            // Like with the opening node, we'll need to check if the
+            // $end_mark has <sen> children to which the closing bracket
+            // should be appended. If not, we'll append the closing bracket to
+            // $end_node itself, just like we did with $start_node.
+            var $closing_node = $end_mark.find('sen').last();
+            if (!$closing_node.prop('tagName')) {
+                $closing_node = $end_mark;
+            }
+            $closing_node.append(append_closing_text);
         }
     });
 
